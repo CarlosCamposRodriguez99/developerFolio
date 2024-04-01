b8c4fb586f7954d7c05452faff7130ef0b32b75f
Luis Campos <jccamposrz@gmail.com>
2024-04-01 17:26:20 -0600

    01-04-24

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Hecho con ❤️ x GaiaSoft")}
+          {emoji("Hecho con ❤️ x TheFields")}
         </p>
         
       </div>
